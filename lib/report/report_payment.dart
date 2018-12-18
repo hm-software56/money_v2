be1df5af571a69ed310ae7f6c013060588c08b8f
daxionginfo@gmail.com <daxionginfo@gmail.com>
2018-12-18 15:19:16 +0700

    fixed

--- lib/report/report_payment.dart
+++ lib/report/report_payment.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:money/login.dart';
 import 'package:money/report/tab_pay_month.dart';
 import 'package:money/report/tab_pay_week.dart';
 import 'package:money/report/tab_pay_year.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 class ReportPayment extends StatefulWidget {
   _ReportPaymentState createState() => _ReportPaymentState();
 }
 
 class _ReportPaymentState extends State<ReportPayment> {
+
+  /*========== Login expired ================*/
+  Future<Null> checkloginexiped() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var now = new DateTime.now();
+    var formatter = new DateFormat('yyyyMMddhm');
+    String formatted = formatter.format(now);
+    if (int.parse(formatted) >= prefs.getInt('time')) {
+      prefs.remove('token');
+      prefs.remove('time');
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Login()));
+    } else {
+      prefs.setInt('time', int.parse(formatted) + 10);
+    }
+  }
+void initState() { 
+  super.initState();
+  checkloginexiped();
+}
+
   @override
   Widget build(BuildContext context) {
     return  DefaultTabController(
--- lib/report/report_recive.dart
+++ lib/report/report_recive.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:money/login.dart';
 import 'package:money/report/tab_pay_month.dart';
 import 'package:money/report/tab_pay_week.dart';
 import 'package:money/report/tab_pay_year.dart';
 import 'package:money/report/tab_recive_month.dart';
 import 'package:money/report/tab_recive_week.dart';
 import 'package:money/report/tab_recive_year.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 class ReportRecive extends StatefulWidget {
   _ReportReciveState createState() => _ReportReciveState();
 }
 
 class _ReportReciveState extends State<ReportRecive> {
+  
+  /*========== Login expired ================*/
+  Future<Null> checkloginexiped() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var now = new DateTime.now();
+    var formatter = new DateFormat('yyyyMMddhm');
+    String formatted = formatter.format(now);
+    if (int.parse(formatted) >= prefs.getInt('time')) {
+      prefs.remove('token');
+      prefs.remove('time');
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Login()));
+    } else {
+      prefs.setInt('time', int.parse(formatted) + 10);
+    }
+  }
+void initState() { 
+  super.initState();
+  checkloginexiped();
+}
   @override
   Widget build(BuildContext context) {
     return  DefaultTabController(
